e649f3e5d56097cc33e8d7175b3304582325ca95
Ahmed Nussair <anussair@gmail.com>
2021-07-15 15:52:34 +0200

    Committed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,1 @@
-import 'view/first_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(EccApp());
-}
-
-class EccApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstScreen(),
-    );
-  }
-}
